c93505398976d9a3dbffe0d290cc6273a95099f7
youxiong <726843014@qq.com>
2018-04-01 15:36:00 +0800

    支付回调测试

--- spring_boot_pay/src/main/java/com/youxiong/Application.java
+++ spring_boot_pay/src/main/java/com/youxiong/Application.java
@@ -2,14 +2,21 @@ package com.youxiong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class Application {
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args){
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class,args);
     }
 }
